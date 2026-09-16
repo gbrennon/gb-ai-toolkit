51ca821c93b3034aafeb1e0c07b5b98c26c33ac4
Glauber Brennon <glauberbrennon@gmail.com>
2026-09-16 15:34:33 -0300

    refactor(hooks): export platform installers

--- src/ai_toolkit/install_hooks/__init__.py
+++ src/ai_toolkit/install_hooks/__init__.py
@@ -1,17 +1,19 @@
+from ai_toolkit.install_hooks.installers.cline_hooks_installer import ClineHooksInstaller
 from ai_toolkit.install_hooks.installers.hook_installer import HookInstaller
-from ai_toolkit.install_hooks.installers.hook_installer_factory import (
-    HookInstallerFactory,
-)
-from ai_toolkit.install_hooks.installers.native_enforcement_installer import (
-    NativeEnforcementInstaller,
+from ai_toolkit.install_hooks.installers.hook_installer_factory import HookInstallerFactory
+from ai_toolkit.install_hooks.installers.omp_hooks_installer import OmpHooksInstaller
+from ai_toolkit.install_hooks.installers.opencode_hooks_installer import (
+    OpenCodeHooksInstaller,
 )
 from ai_toolkit.install_hooks.installers.pi_hooks_installer import PiHooksInstaller
 from ai_toolkit.install_hooks.main import install, main
 
 __all__ = [
+    "ClineHooksInstaller",
     "HookInstaller",
     "HookInstallerFactory",
-    "NativeEnforcementInstaller",
+    "OmpHooksInstaller",
+    "OpenCodeHooksInstaller",
     "PiHooksInstaller",
     "install",
     "main",
